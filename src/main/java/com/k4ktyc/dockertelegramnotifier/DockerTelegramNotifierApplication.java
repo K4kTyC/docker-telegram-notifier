94136120be497dfb7467be23d076350466878a4e
K4kTyC <47088033+K4kTyC@users.noreply.github.com>
2022-11-17 17:45:08 +0300

    init

---
+++ src/main/java/com/k4ktyc/dockertelegramnotifier/DockerTelegramNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.k4ktyc.dockertelegramnotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTelegramNotifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerTelegramNotifierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/k4ktyc/dockertelegramnotifier/DockerTelegramNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k4ktyc.dockertelegramnotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTelegramNotifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
